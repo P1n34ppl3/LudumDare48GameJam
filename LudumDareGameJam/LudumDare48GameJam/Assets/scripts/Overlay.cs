04d7c0b4c596619511244e419021839438e28a7c
P1n34ppl3 <lander.deveuster@telenet.be>
2021-04-24 23:20:12 +0200

    quick animation fix

--- LudumDareGameJam/LudumDare48GameJam/Assets/scripts/Overlay.cs
+++ LudumDareGameJam/LudumDare48GameJam/Assets/scripts/Overlay.cs
@@ -33,6 +33,8 @@ public class Overlay : MonoBehaviour
         playerMovement.enabled = true;
 
         playerLook.enabled = true;
+
+        playerMovement.SleepingAnimation();
     }
 
     public void QuitGame()
