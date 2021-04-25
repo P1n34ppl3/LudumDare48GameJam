127f4a2b51b8517a079b269a9f2c34df37240d4e
P1n34ppl3 <lander.deveuster@telenet.be>
2021-04-25 23:03:43 +0200

    menu update

--- LudumDareGameJam/LudumDare48GameJam/Assets/scripts/Overlay.cs
+++ LudumDareGameJam/LudumDare48GameJam/Assets/scripts/Overlay.cs
@@ -2,53 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Overlay : MonoBehaviour
 {
-    public GameObject menu;
     public GameObject tipsAndObjectives;
     public TextMeshProUGUI objectiveText;
-    public GameObject player;
-    public PlayerMovement playerMovement;
-    public GameObject mainCamera;
-    public PlayerLook playerLook;
     public int objectiveNumber = 0;
-
-
-    void Start()
-    {   
-        Time.timeScale = 0;
-
-        menu.SetActive(true);
-
-        playerMovement = player.GetComponent<PlayerMovement>();
-        playerMovement.enabled = false;
-
-        playerLook = mainCamera.GetComponent<PlayerLook>();
-        playerLook.enabled = false;
-    }
 
     void Update()
     {
         UpdateTipsAndObjectives();
-    }
-
-    public void PlayGame()
-    {   
-        Time.timeScale = 1;
-
-        menu.SetActive(false);
-
-        playerMovement.enabled = true;
-
-        playerLook.enabled = true;
-
-        EnableTipsAndObjectives();
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
     }
 
     public void EnableTipsAndObjectives()
