02a91f02c55eb7a9e170f9dfdf6e0b4fff8664ee
NewryBenson <76691448+NewryBenson@users.noreply.github.com>
2021-04-26 23:03:41 +0200

    Update Progression.cs

--- LudumDareGameJam/LudumDare48GameJam/Assets/scripts/Progression.cs
+++ LudumDareGameJam/LudumDare48GameJam/Assets/scripts/Progression.cs
@@ -81,6 +81,50 @@ public class Progression : MonoBehaviour
         yield return new WaitUntil(() => Input.GetKey(KeyCode.T) == true);
         StoryHolder.SetActive(false);
         Gamestage = 5;
+        yield return new WaitUntil(() => Gamestage == 6);
+        PilCollection.pils = 0;
+        yield return new WaitForSeconds(wait);
+        StoryHolder.SetActive(true);
+        Story.text = Text[10];
+        yield return new WaitUntil(() => Input.GetKey(KeyCode.T) == true);
+        StoryHolder.SetActive(false);
+        yield return new WaitForSeconds(wait);
+        StoryHolder.SetActive(true);
+        Story.text = Text[11];
+        yield return new WaitUntil(() => Input.GetKey(KeyCode.T) == true);
+        StoryHolder.SetActive(false);
+        Gamestage = 7;
+        yield return new WaitUntil(() => PilCollection.pils >= 3);
+        StoryHolder.SetActive(true);
+        Story.text = Text[12];
+        yield return new WaitUntil(() => Input.GetKey(KeyCode.T) == true);
+        StoryHolder.SetActive(false);
+        Gamestage = 8;
+        yield return new WaitUntil(() => Gamestage == 9);
+        PilCollection.pils = 0;
+        yield return new WaitForSeconds(wait);
+        StoryHolder.SetActive(true);
+        Story.text = Text[13];
+        yield return new WaitUntil(() => Input.GetKey(KeyCode.T) == true);
+        StoryHolder.SetActive(false); 
+        yield return new WaitForSeconds(wait);
+        StoryHolder.SetActive(true);
+        Story.text = Text[14];
+        yield return new WaitUntil(() => Input.GetKey(KeyCode.T) == true);
+        StoryHolder.SetActive(false); 
+        yield return new WaitForSeconds(wait);
+        StoryHolder.SetActive(true);
+        Story.text = Text[15];
+        yield return new WaitUntil(() => Input.GetKey(KeyCode.T) == true);
+        StoryHolder.SetActive(false);
+        Gamestage = 10;
+        yield return new WaitForSeconds(10);
+        StoryHolder.SetActive(true);
+        Story.text = Text[15];
+        yield return new WaitUntil(() => Input.GetKey(KeyCode.T) == true);
+        StoryHolder.SetActive(false);
+
+
 
     }
 
